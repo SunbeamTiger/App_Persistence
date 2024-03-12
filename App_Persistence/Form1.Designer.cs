7e7a9d681c8fac9b29baef42a21bde48a0b76c23
DanHagen <dan.hagen@centralia.edu>
2024-03-12 09:44:42 -0700

    Add project files.

---
+++ App_Persistence/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace App_Persistence
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chkColor = new System.Windows.Forms.CheckBox();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // chkColor
+            // 
+            this.chkColor.AutoSize = true;
+            this.chkColor.Location = new System.Drawing.Point(215, 106);
+            this.chkColor.Name = "chkColor";
+            this.chkColor.Size = new System.Drawing.Size(165, 30);
+            this.chkColor.TabIndex = 0;
+            this.chkColor.Text = "Change Color";
+            this.chkColor.UseVisualStyleBackColor = true;
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(215, 49);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(165, 41);
+            this.btnColor.TabIndex = 1;
+            this.btnColor.Text = "Change Color";
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 337);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.chkColor);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkColor;
+        private System.Windows.Forms.Button btnColor;
+    }
+}
+
---
+++ App_Persistence/Form1.cs
@@ -0,0 +1,58 @@
+﻿using App_Persistence.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App_Persistence
+{
+    public partial class Form1 : Form
+    {
+        private Color _chosenColor;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getSettings()
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int FW = Settings.Default.FormWidth;
+            int FH = Settings.Default.FormHeight;
+            _chosenColor = Settings.Default.FormColor;
+
+             
+            this.Height = FH;   
+            bool ColorizeIt = Settings.Default.ShowColor;
+            if (ColorizeIt==true)
+            {
+                this.BackColor = _chosenColor; 
+                chkColor.Checked = true;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Settings.Default.FormColor = _chosenColor;
+            Settings.Default.FormWidth = this.Width;
+            Settings.Default.FormHeight = this.Height;
+            Settings.Default.ShowColor = chkColor.Checked;  
+            Settings.Default.Save();
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog dlg = new ColorDialog();    
+            dlg.ShowDialog();  
+            _chosenColor = dlg.Color;   
+        }
+    }
+}
